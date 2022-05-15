cb1507e00bcefd94c78df546ab234569269c7c91
Seppe Dekeyser <seppe@dva.be>
2022-05-15 22:12:35 +0200

    Update the random module so that it displays the correct help for the random command

--- modules/userEmbeds.js
+++ modules/userEmbeds.js
@@ -1,18 +1,18 @@
 exports.commands = [
-    "pickRandom"
+    "random"
 ];
 
 const splitOptions = (options) => {
     return options.split("\"").map(item => item.trim()).filter(item => item !== "");
 }
 
-exports.pickRandom = {
+exports.random = {
     description: "Picks one of the provided options",
     usage: "!random \"<option one>\" \"<option two>\" ...",
     aliases: ["random"],
     process: (bot, message, options) => {
         if (options.length <= 0) {
-            message.reply(`Usage: \`${this.pickRandom.usage}\``);
+            message.reply(`Usage: \`${this.random.usage}\``);
             return;
         }
 
